3fc759c5747859c70a111bbfaa88ee03d8529f56
ncw <whitesky999@naver.com>
2020-06-10 22:40:14 +0900

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# League of Legend tournament stat comparison application
-
-- for react-redux-thunk-typescript practice
-- LCK, LPL, LEC, LCS and international tournament
-- data reference : https://gol.gg/esports/home/
---
+++ precache-manifest.0e7942264286dd6200e56488d0228936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4971dc63d6008bceef14618b03d66f",
+    "url": "/LOL-tournament-stat-comparison/index.html"
+  },
+  {
+    "revision": "84f8dcf923685b0ec5f8",
+    "url": "/LOL-tournament-stat-comparison/static/css/2.698590b6.chunk.css"
+  },
+  {
+    "revision": "a069a33c4ca9ab7cd159",
+    "url": "/LOL-tournament-stat-comparison/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "84f8dcf923685b0ec5f8",
+    "url": "/LOL-tournament-stat-comparison/static/js/2.72a824f9.chunk.js"
+  },
+  {
+    "revision": "7a80e51e08859d58c1022a3d987bb0da",
+    "url": "/LOL-tournament-stat-comparison/static/js/2.72a824f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a069a33c4ca9ab7cd159",
+    "url": "/LOL-tournament-stat-comparison/static/js/main.94beae6f.chunk.js"
+  },
+  {
+    "revision": "020359c9dc6f72f507d6",
+    "url": "/LOL-tournament-stat-comparison/static/js/runtime-main.2270b416.js"
+  }
+]);
