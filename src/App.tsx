a9aa00bdbd76f8dd17b161de6c176075fbef7e3b
ncw <whitesky999@naver.com>
2020-06-22 16:48:40 +0900

    favicon issue temp fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<Route exact path="/" component={Main}/>
-			<Route exact path="/record" component={Record}/>
+			<Route path="/record" component={Record}/>
 		</BrowserRouter>
 	);
 }
