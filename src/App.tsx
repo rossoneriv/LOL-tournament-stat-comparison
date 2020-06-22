45e7a319c840c75b88feaed0a0d76241dc408083
ncw <whitesky999@naver.com>
2020-06-22 17:48:17 +0900

    Change to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 
 import Main from './components/comparison/Comparison';
 import Record from './components/record/Record';
@@ -7,10 +7,10 @@ import Record from './components/record/Record';
 function App() {
 
 	return (
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
+		<HashRouter basename={process.env.PUBLIC_URL}>
 			<Route exact path="/" component={Main}/>
 			<Route path="/record" component={Record}/>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
